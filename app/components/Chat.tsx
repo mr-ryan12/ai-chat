4dde18a2bc303a4e7f47393fa4600615b545a540
Ryan McBride <mr.ryan444@gmail.com>
2025-04-12 22:51:26 -0600

    Add casing to first letter of output

--- app/components/Chat.tsx
+++ app/components/Chat.tsx
@@ -44,9 +44,11 @@ export default function Chat() {
       let currentIndex = 0;
       const interval = setInterval(() => {
         if (currentIndex < actionData.words!.length) {
-          setStreamingResponse(
-            (prev) => prev + actionData.words![currentIndex] + " "
-          );
+          let word = actionData.words![currentIndex];
+          if (currentIndex === 0) {
+            word = word.charAt(0).toUpperCase() + word.slice(1);
+          }
+          setStreamingResponse((prev) => prev + word + " ");
           currentIndex++;
         } else {
           clearInterval(interval);
