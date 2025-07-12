3a22e1e751329b04dd859b51dab86700a789177e
Ryan McBride <mr.ryan444@gmail.com>
2025-07-11 22:23:41 -0600

    Destructure checker plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
-import checker from "vite-plugin-checker";
+import { checker } from "vite-plugin-checker";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 declare module "@remix-run/node" {
